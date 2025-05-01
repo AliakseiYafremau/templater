267a2942502e5d499dd20828432e10de116d5da5
Aliaksei Yafremau <aleksey.yafremau@gmail.com>
2025-05-01 19:15:44 +0100

    Add list function

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,16 @@
 module Main where
 
 import Options.Applicative
-import System.Directory (createDirectoryIfMissing, doesFileExist, getHomeDirectory, 
+import System.Directory (createDirectoryIfMissing, doesFileExist, getHomeDirectory,
                          doesDirectoryExist, listDirectory, copyFile)
 import System.FilePath ((</>), takeFileName)
 import qualified Data.ByteString as BS
-import Control.Monad (when)
+import Control.Monad (when, unless)
+import Data.List (sort)
 
 data Command = Store { target :: FilePath }
              | Load  { templateName :: String, outputPath :: Maybe FilePath }
+             | List
              deriving (Show)
 
 storeCommand :: Parser Command
@@ -30,18 +32,21 @@ loadCommand = Load
        <> metavar "FILE"
        <> help "Output file path" ))
 
+listCommand :: Parser Command
+listCommand = pure List
+
 commandParser :: Parser Command
 commandParser = subparser
     ( command "store" (info storeCommand (progDesc "Store a file as template"))
    <> command "load"  (info loadCommand  (progDesc "Load a template to file"))
+   <> command "list"  (info listCommand  (progDesc "List all available templates"))
     )
 
 getTemplatesDir :: IO FilePath
 getTemplatesDir = do
     home <- getHomeDirectory
-    return $ home </> "templates"
+    return $ home </> ".templater"
 
--- Сохраняем файл как шаблон
 storeTemplate :: FilePath -> IO ()
 storeTemplate srcFile = do
     templatesDir <- getTemplatesDir
@@ -58,7 +63,6 @@ storeTemplate srcFile = do
         else
             putStrLn $ "Error: Source file does not exist: " ++ srcFile
 
--- Загружаем шаблон
 loadTemplate :: String -> Maybe FilePath -> IO ()
 loadTemplate templateName outputPath = do
     templatesDir <- getTemplatesDir
@@ -74,20 +78,41 @@ loadTemplate templateName outputPath = do
             putStrLn $ "Template loaded to: " ++ dest
         else do
             putStrLn $ "Error: Template not found: " ++ templateName
-            -- Показываем доступные шаблоны
-            dirExists <- doesDirectoryExist templatesDir
-            when dirExists $ do
-                templates <- listDirectory templatesDir
-                putStrLn "Available templates:"
-                mapM_ putStrLn templates
+            showAvailableTemplates
+
+listTemplates :: IO ()
+listTemplates = do
+    templatesDir <- getTemplatesDir
+    dirExists <- doesDirectoryExist templatesDir
+    
+    if dirExists
+        then do
+            templates <- listDirectory templatesDir
+            if null templates
+                then putStrLn "No templates available"
+                else do
+                    putStrLn "Available templates:"
+                    mapM_ putStrLn (sort templates)
+        else putStrLn "Templates directory does not exist"
+
+showAvailableTemplates :: IO ()
+showAvailableTemplates = do
+    templatesDir <- getTemplatesDir
+    dirExists <- doesDirectoryExist templatesDir
+    when dirExists $ do
+        templates <- listDirectory templatesDir
+        unless (null templates) $ do
+            putStrLn "Available templates:"
+            mapM_ putStrLn (sort templates)
 
 main :: IO ()
 main = do
     cmd <- execParser (info (commandParser <**> helper) 
         ( fullDesc
         <> progDesc "Command line tool for storing and loading templates"
-        <> header "Template Storage Tool" ))
+        <> header "Template Storage Tool - v1.0" ))
 
     case cmd of
         Store target    -> storeTemplate target
         Load name output -> loadTemplate name output
+        List           -> listTemplates
